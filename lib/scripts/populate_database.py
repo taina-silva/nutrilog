6ad77085f9fe620c0fb1cd97717f25b0b8173e19
Tainá Santos <taina@biomainvestimentos.com>
2024-02-06 18:26:18 -0300

    refactor: updating script to populate database at firestore

--- lib/scripts/populate_database.py
+++ lib/scripts/populate_database.py
@@ -7,45 +7,44 @@ cred = credentials.Certificate("lib/scripts/service_account_key_dev.json")
 app = firebase_admin.initialize_app(cred)
 client = firestore.client()
 
-with open('assets/jsons/physical_activities.json') as f:
+with open("assets/jsons/physical_activities.json") as f:
     data = json.load(f)
 
-    types = data['types']
-    physical_activities = data['physical-activities']
+    types = data["types"]
+    physical_activities = data["physical-activities"]
 
-    docs = client.collection('physical-activities').list_documents()
+    docs = client.collection("physical-activities").list_documents()
 
     for doc in docs:
-       doc.delete()
+        doc.delete()
 
-    doc_ref = client.collection('physical-activities').document('types')
-    doc_ref.set({'types': types})
+    doc_ref = client.collection("physical-activities").document("types")
+    doc_ref.set({"types": types})
 
     for item in physical_activities:
-      print(item)
-      doc_ref = client.collection('physical-activities').document()
-      doc_ref.set({'name': item['name'], 'type': item['type']})
+        print(item)
+        doc_ref = client.collection("physical-activities").document()
+        doc_ref.set({"type": item["type"], "physical-activities": item["pA"]})
 
-print('\nDone uploading physical activities data to firesore!\n')
+print("\nDone uploading physical activities data to firesore!\n")
 
-with open('assets/jsons/nutrition.json') as f:
+with open("assets/jsons/nutrition.json") as f:
     data = json.load(f)
 
-    types = data['types']
-    nutrition = data['nutrition']
+    types = data["types"]
+    nutrition = data["nutrition"]
 
-    docs = client.collection('nutrition').list_documents()
+    docs = client.collection("nutrition").list_documents()
 
     for doc in docs:
-       doc.delete()
+        doc.delete()
 
-    doc_ref = client.collection('nutrition').document('types')
-    doc_ref.set({'types': types})
+    doc_ref = client.collection("nutrition").document("types")
+    doc_ref.set({"types": types})
 
     for item in nutrition:
-      print(item)
-      for i in item['food']:
-        doc_ref = client.collection('nutrition').document()
-        doc_ref.set({'name': i, 'type': item['type']})
+        print(item)
+        doc_ref = client.collection("nutrition").document()
+        doc_ref.set({"type": item["type"], "nutrition": item["food"]})
 
-print('\nDone uploading nutrition data to firesore!\n')
+print("\nDone uploading nutrition data to firesore!\n")
